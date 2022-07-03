00b6caac46bfe88e4d44d5ef7e49b7f9f415d23c
Ahmad <fire_emblem_1@hotmail.com>
2022-07-03 20:20:37 +0300

    task done

--- madlibs.py
+++ madlibs.py
@@ -3,10 +3,11 @@ from unicodedata import name
 
 
 def main():
-	name = input ("enter name: ") 
+	name = input ("enter name: ").capitalize()
 	time = input ("enter time:")
 	item =input ("enter item:")
-	scream = input ("scream here:")
-	print("hi my name is "  + name, "it is currently "  + time, "I am currently carrying my " + item, "but just incase you didnt hear me, MY NAME IS " + scream)
+	scream = input ("scream here:").upper()
+	action = input ("do an action:")
+	print (f"hi my name is  {name}, it is currently {time}, I am currently carrying my  {item}, but just incase you didnt hear me, MY NAME IS {scream}, and im about to go {action} ")
 if __name__ == '__main__':
 	main()
