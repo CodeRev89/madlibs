d4e2469a4a71cb3b5c1ae2ed390be23b715daebc
Ahmad <fire_emblem_1@hotmail.com>
2022-07-03 19:56:27 +0300

    task done

--- madlibs.py
+++ madlibs.py
@@ -1,7 +1,12 @@
+from typing import ItemsView
+from unicodedata import name
+
+
 def main():
-	# write your code here
-
-
-
+	name = input ("enter name: ") 
+	time = input ("enter time:")
+	item =input ("enter item:")
+	scream = input ("scream here:")
+	print("hi my name is "  + name, "it is currently "  + time, "I am currently carrying my " + item, "but just incase you didnt hear me, MY NAME IS " + scream)
 if __name__ == '__main__':
 	main()
